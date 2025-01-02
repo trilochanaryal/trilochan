6cad2702839666d1bad4e9d191fedcad3d303ba7
trylow10 <aryaltrylowchan@gmail.com>
2025-01-02 23:23:03 +0545

    add Prettier configuration, remove unused SVG files, and update Next.js and Tailwind CSS configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {
+  darkMode: 'class',
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './public/**/*.svg'],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      // fontFamily: {
+      //   sans: ['var(--font-geist-sans)'],
+      //   mono: ['var(--font-geist-mono)'],
+      // },
+      typography: {
+        quoteless: {
+          css: {
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+          },
+        },
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require('@tailwindcss/typography')],
+};
