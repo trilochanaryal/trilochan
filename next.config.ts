7928c8c4ef7dd8463f7b3aab2c4813d1427d36e4
trylow10 <aryaltrylowchan@gmail.com>
2025-01-03 21:40:26 +0545

    refactor Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,40 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   async rewrites() {
     return [
       {
-        source: "/rss.xml",
-        destination: "/feed/rss.xml",
+        source: '/rss.xml',
+        destination: '/feed/rss.xml',
       },
       {
-        source: "/atom.xml",
-        destination: "/feed/atom.xml",
+        source: '/atom.xml',
+        destination: '/feed/atom.xml',
       },
       {
-        source: "/feed.json",
-        destination: "/feed/feed.json",
+        source: '/feed.json',
+        destination: '/feed/feed.json',
       },
       {
-        source: "/rss",
-        destination: "/feed/rss.xml",
+        source: '/rss',
+        destination: '/feed/rss.xml',
       },
       {
-        source: "/feed",
-        destination: "/feed/rss.xml",
+        source: '/feed',
+        destination: '/feed/rss.xml',
       },
       {
-        source: "/atom",
-        destination: "/feed/atom.xml",
+        source: '/atom',
+        destination: '/feed/atom.xml',
       },
       {
-        source: "/json",
-        destination: "/feed/feed.json",
+        source: '/json',
+        destination: '/feed/feed.json',
       },
     ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
