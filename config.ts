ab97f46a0827fd0b3d2d49b1c50c83e0ddb1e47f
trylow10 <aryaltrylowchan@gmail.com>
2025-01-03 22:08:59 +0545

    add configuration file for metadata and social links

---
+++ config.ts
@@ -0,0 +1,15 @@
+export const metaData = {
+  baseUrl: 'https://atrilochan.com.np',
+  title: "Trilochan's Portfolio",
+  name: 'Trilochan Aryal',
+  ogImage: '/opengraph-image.png',
+  description: 'This is my portfolio built using next js',
+};
+
+export const socialLinks = {
+  twitter: 'https://x.com/TrilochanArl',
+  github: 'https://github.com/trilochanaryal',
+  instagram: 'https://www.instagram.com/trylow.arl',
+  linkedin: 'https://www.linkedin.com/in/trilochanaryal',
+  email: 'mailto:aryaltrylowchan@gmail.com',
+};
