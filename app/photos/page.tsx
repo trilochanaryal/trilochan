67566a033a7fa91cd3eb91734bda476f9231f7e8
trylow10 <aryaltrylowchan@gmail.com>
2025-01-09 00:02:07 +0545

    update Photos component to reduce image grid columns

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -12,61 +12,28 @@ export default function Photos() {
     <section>
       <h1 className="mb-8 text-2xl font-medium tracking-tight">Photos</h1>
       <ImageGrid
-        columns={3}
+        columns={2}
         images={[
           {
             src: '/photos/photo1.jpg',
             alt: 'florista hut',
-            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/florista.jpg',
+            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/florista.jpg?raw=true',
           },
           {
             src: '/photos/photo2.jpg',
             alt: 'way to muktinath',
-            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/waytomustang.jpg',
+            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/waytomustang.jpg?raw=true',
           },
           {
             src: '/photos/photo3.jpg',
             alt: 'rupse waterfall',
-            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/rupse.jpg',
+            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/rupse.jpg?raw=true',
           },
           {
             src: '/photos/photo4.jpg',
             alt: 'shot taken at funpark',
-            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/funpark.jpg',
+            href: 'https://github.com/trilochanaryal/assets/blob/main/portfolio-photos/funpark.jpg?raw=true',
           },
-        ]}
-      />
-
-      {/* {
-            src: '/photos/photo5.jpg',
-            alt: 'Taj Mahal',
-            href: 'https://unsplash.com/photos/taj-mahal-india-IPlPkWPJ2fo',
-          },
-          {
-            src: '/photos/photo6.jpg',
-            alt: 'Colosseum',
-            href: 'https://unsplash.com/photos/brown-concrete-building-under-blue-sky-during-daytime-3cyBR1rIJmA?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash',
-          }, */}
-
-      <ImageGrid
-        columns={2}
-        images={[
-          { src: '/photos/photo1.jpg', alt: 'Roman columns' },
-          { src: '/photos/photo2.jpg', alt: 'Big Ben' },
-          { src: '/photos/photo3.jpg', alt: 'Sacré-Cœur Basilica' },
-          { src: '/photos/photo4.jpg', alt: 'Eiffel Tower' },
-        ]}
-      />
-
-      <ImageGrid
-        columns={4}
-        images={[
-          { src: '/photos/photo1.jpg', alt: 'Roman columns' },
-          { src: '/photos/photo2.jpg', alt: 'Big Ben' },
-          { src: '/photos/photo3.jpg', alt: 'Sacré-Cœur Basilica' },
-          { src: '/photos/photo4.jpg', alt: 'Eiffel Tower' },
-          // { src: '/photos/photo5.jpg', alt: 'Taj Mahal' },
-          // { src: '/photos/photo6.jpg', alt: 'Colosseum' },
         ]}
       />
     </section>
