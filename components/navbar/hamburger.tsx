1cb2f1d4dc9d3d33ea4e29b89dd814ae26e34cbc
trylow10 <aryaltrylowchan@gmail.com>
2025-01-10 16:53:30 +0545

    refactor hamburger component; export function and update button styles for better visibility

--- components/navbar/hamburger.tsx
+++ components/navbar/hamburger.tsx
@@ -8,7 +8,7 @@ type Props = {
   onToggleHamburger: () => void;
 };
 
-function Hamburger({ isMenuOpen, onToggleHamburger }: Props) {
+export function Hamburger({ isMenuOpen, onToggleHamburger }: Props) {
   return (
     <button
       className="z-50 flex flex-col md:hidden"
@@ -17,24 +17,16 @@ function Hamburger({ isMenuOpen, onToggleHamburger }: Props) {
     >
       <span
         className={cn(
-          'h-0.5 w-6 bg-foreground transition-all duration-300 ease-in-out',
+          'h-0.5 w-6 bg-black dark:bg-white transition-all duration-300 ease-in-out',
           isMenuOpen && 'translate-y-1.5 rotate-45',
         )}
       ></span>
       <span
         className={cn(
-          'mt-1.5 h-0.5 w-6 bg-foreground transition-all duration-300 ease-in-out',
-          isMenuOpen && 'opacity-0',
-        )}
-      ></span>
-      <span
-        className={cn(
-          'mt-1.5 h-0.5 w-6 bg-foreground transition-all duration-300 ease-in-out',
+          'mt-1.5 h-0.5 w-5 bg-black dark:bg-white transition-all duration-300 ease-in-out',
           isMenuOpen && '-translate-y-1.5 -rotate-45',
         )}
       ></span>
     </button>
   );
 }
-
-export default Hamburger;
