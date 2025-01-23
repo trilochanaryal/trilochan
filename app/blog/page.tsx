041ffa383352dfd5a364a50f75f4849146d8e7b7
trylow10 <aryaltrylowchan@gmail.com>
2025-01-23 22:51:01 +0545

    refactor Blog component; restructure layout to include tab navigation for Blog and Medium

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,45 +1,29 @@
-import Link from 'next/link';
-import { formatDate, getBlogPosts } from '@/lib/posts';
+import BlogLayout from '@/components/tab/blog-tab';
+import MediumLayout from '@/components/tab/medium-tab';
+import TabLayout from '@/components/tab/tab-layout';
+import { Tab } from '@/types';
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Nextfolio Blog',
+  title: 'Blog & Projects',
+  description: 'Nextfolio Blog and Projects',
 };
 
-export default function BlogPosts() {
-  const allBlogs = getBlogPosts();
+export default function Blog() {
+  const tabs: Tab[] = [
+    {
+      label: 'Blog',
+      content: <BlogLayout />,
+    },
+    {
+      label: 'Medium',
+      content: <MediumLayout />,
+    },
+  ];
 
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">Our Blog</h1>
-      <div>
-        {allBlogs
-          .sort((a, b) => {
-            if (
-              new Date(a.metadata.publishedAt) >
-              new Date(b.metadata.publishedAt)
-            ) {
-              return -1;
-            }
-            return 1;
-          })
-          .map((post) => (
-            <Link
-              key={post.slug}
-              className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80"
-              href={`/blog/${post.slug}`}
-            >
-              <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
-                <p className="text-black dark:text-white tracking-tight">
-                  {post.metadata.title}
-                </p>
-                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                  {formatDate(post.metadata.publishedAt, false)}
-                </p>
-              </div>
-            </Link>
-          ))}
-      </div>
+    <section className="max-w-2xl mx-auto px-4 py-12">
+      <h1 className="text-3xl font-bold mb-8 text-center">Our Blog</h1>
+      <TabLayout tabs={tabs} />
     </section>
   );
 }
