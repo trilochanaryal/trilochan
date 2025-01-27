22c3b7651150673e6ff58909b9266214a8c98464
trylow10 <aryaltrylowchan@gmail.com>
2025-01-27 23:48:50 +0545

    add Metadata and Post types for blog post structure

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,23 @@ export interface Tab {
   label: string;
   content: React.ReactNode;
 }
+
+export type Metadata = {
+  title: string;
+  publishedAt: string;
+  summary: string;
+  tags: string;
+  image?: string;
+};
+
+export interface Post {
+  metadata: {
+    title: string;
+    publishedAt: string;
+    tags: string[];
+    description?: string;
+  };
+  slug: string;
+  content: string;
+  source: 'medium' | 'local';
+}
